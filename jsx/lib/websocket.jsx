dd28eb8ea03a53fb810fb37e605bdf89b06544c8
grayzone <grayzone137@gmail.com>
2017-07-03 12:00:27 +0800

    show the message in the page.

--- jsx/lib/websocket.jsx
+++ jsx/lib/websocket.jsx
@@ -1,18 +1,34 @@
 import React from "react";
-import { Row, Col, Input, Button } from "antd";
+import { Row, Col, Input, Button, Timeline } from "antd";
 import $ from "jquery";
 import URL from "url";
 
 // var socket; //= new WebSocket('ws://'+ window.location.host + "/ws/join?uname=" + URL.parse(window.location.href, true).query.uname);
 
+class ChatLine extends React.Component {
+  render() {
+    const chatMsg = [];
+    for (let i = 0; i < this.props.data.length; i++) {
+      var msg = this.props.data[i];
+      var l = <Timeline.Item>{msg}</Timeline.Item>;
+      chatMsg.push(l);
+    }
+    return (
+      <Timeline>
+        {chatMsg}
+      </Timeline>
+    );
+  }
+}
+
 export default class WebSocketPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: ""
+      message: "",
+      list: []
     };
   }
-
   handleMessageChange = e => {
     this.setState({
       message: e.target.value
@@ -20,13 +36,13 @@ export default class WebSocketPage extends React.Component {
   };
   handleSendClick = e => {
     console.log("send the message using web socket.");
-    console.log(this.connection);
+    //    console.log(this.connection);
     this.connection.send(this.state.message);
     console.log("click send:", this.state.message);
   };
 
   componentDidMount() {
-    console.log("web socket:", this.connection);
+    //    console.log("web socket:", this.connection);
     var p = URL.parse(window.location.href, true);
     var uname = p.query.uname;
     var wsurl = "ws://" + window.location.host + "/ws/join?uname=" + uname;
@@ -41,18 +57,27 @@ export default class WebSocketPage extends React.Component {
     this.connection.onmessage = event => {
       var data = JSON.parse(event.data);
       console.log("message:", data);
+      var msg;
       switch (data.Type) {
         case 0:
           if (data.User == uname) {
-            console.log("you joined the chat room.");
+            msg = "you joined the chat room.";
           } else {
-            console.log(uname + " joined the char room.");
+            msg = uname + " joined the char room.";
           }
+          break;
         case 1:
-          console.log(data.User + " left the chat room");
+          msg = data.User + " left the chat room";
+          break;
         case 2:
-          console.log(data.User + " message:" + data.Content);
+          msg = data.User + " message:" + data.Content;
+          break;
       }
+      console.log(msg);
+      this.setState(prev => ({
+        list: prev.list.concat(msg)
+      }));
+      console.log(this.state.list);
     };
   }
 
@@ -67,6 +92,9 @@ export default class WebSocketPage extends React.Component {
         </Col>
         <Col>
           <Button onClick={this.handleSendClick}>Send</Button>
+        </Col>
+        <Col>
+        <ChatLine data={this.state.list} />
         </Col>
       </Row>
     );
